efea301332685f96a3d0937f35447d090c6f9927
HeyIamJames <jameshemmaplardh@gmail.com>
2015-12-17 15:24:16 -0800

    grab source code from craigs

--- crawler2.py
+++ crawler2.py
@@ -1,2 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+
+def trade_spider(max_pages):
+    page = 1
+    while page < max_pages:
+        url = 'http://seattle.craigslist.org/search/cta' +str(page)
+        source_code = requests.get(url)
