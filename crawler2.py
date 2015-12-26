14f856b6e4d64e55e0a1a90e461748e2e82e00dd
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-12-26 15:47:10 -0800

    Update crawler2.py

--- crawler2.py
+++ crawler2.py
@@ -6,3 +6,9 @@ def trade_spider(max_pages):
     while page < max_pages:
         url = 'http://seattle.craigslist.org/search/cta' +str(page)
         source_code = requests.get(url)
+        plain_text = source_code.text
+        #plain text is source code from web space
+        soup = BeautifulSoup(plain_text)
+        
+        for link in soup.findAll('a', ):
+            
