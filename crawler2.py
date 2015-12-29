4542ae8bd22113a8573bb52b5bf0a4c3e4fa0483
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-12-28 20:48:20 -0800

    Update crawler2.py

--- crawler2.py
+++ crawler2.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def trade_spider(max_pages):
-    page = 1
+    page = 0
     while page < max_pages:
         url = 'http://seattle.craigslist.org/search/cta' +str(page)
         source_code = requests.get(url)
