db89b9eabfa8ece5cd6a1e16fd2bbb424aeebe3d
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-01-05 22:31:18 -0800

    Update PacketSniffer.py

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -7,3 +7,8 @@ def ethernet_frame(data):
   dest_mac, src_mac, proto = struct, unpack('! 6s 5s H', data[:14])
 #first 14 bytes give dest, source and type
   return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+def get_mac_addr(bytes_addr):
+  bytes_str = map( {}.format)
+  mac_addr = 
+  
