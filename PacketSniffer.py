52c9a607b38a26c2e9c1ac0e0f49701624eb2b08
HeyIamJames <jameshemmaplardh@gmail.com>
2016-02-16 12:26:10 -0800

    lint

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -76,24 +76,22 @@ while True:
     print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
      
     tcp_header = packet[iph_length:iph_length+20]
-     
+
     #now unpack them :)
     tcph = unpack('!HHLLBBHHH' , tcp_header)
-     
+
     source_port = tcph[0]
     dest_port = tcph[1]
     sequence = tcph[2]
     acknowledgement = tcph[3]
     doff_reserved = tcph[4]
     tcph_length = doff_reserved >> 4
-     
     print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
-     
+
     h_size = iph_length + tcph_length * 4
     data_size = len(packet) - h_size
-     
+
     #get data from the packet
     data = packet[h_size:]
-     
-    print 'Data : ' + data
 
+    print 'Data : ' + data
