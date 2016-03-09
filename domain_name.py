133ab898a2b8e007cc2941bf34f1a4d42444362e
HeyIamJames <jameshemmaplardh@gmail.com>
2016-03-09 12:41:00 -0800

    spelling error

--- domain_name.py
+++ domain_name.py
@@ -1,11 +1,9 @@
 #pip > add req
-from tld import get_tkd
+from tld import get_tld
 
 #get toplevel dn
 def get_domain_name(url):
     domain_name = get_tld(url)
     return domain_name
 
-print(get_domain_name(thejameshemmaplardh.com))
-
-#thejameshemmaplardh.com
+print(get_domain_name('https://www.thejameshemmaplardh.com'))
