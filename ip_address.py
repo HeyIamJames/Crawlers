92a8c83db524a291b514bb9dce0eeec3817794d1
HeyIamJames <jameshemmaplardh@gmail.com>
2016-03-09 12:57:41 -0800

    return using socket

--- ip_address.py
+++ ip_address.py
@@ -1,10 +1,12 @@
 import os
 
 def get_ip_address(url):
-    command = "host" + url
+    command = "host " + url
     #init new process
-    process - os.open(command)
+    process = os.popen(command)
     results = str(process.read())
-    marker = results.find('has address') + 12 
+    marker = results.find('has address') + 12
     #sice string pre ip
     return results[marker:].splitlines()[0]
+
+print(get_ip_address('google.com'))
