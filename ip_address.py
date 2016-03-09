1d7b5ca0a1bd32bf575b71f471e3ccb759dfb0c6
HeyIamJames <jameshemmaplardh@gmail.com>
2016-03-09 12:49:17 -0800

    only return ip

--- ip_address.py
+++ ip_address.py
@@ -3,4 +3,8 @@ import os
 def get_ip_address(url):
     command = "host" + url
     #init new process
-    
+    process - os.open(command)
+    results = str(process.read())
+    marker = results.find('has address') + 12 
+    #sice string pre ip
+    return results[marker:].splitlines()[0]
