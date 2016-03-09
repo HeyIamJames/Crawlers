7a0548528e42a621abf40c3d5d4e89a5c69d08d6
HeyIamJames <jameshemmaplardh@gmail.com>
2016-03-09 12:08:34 -0800

    create dir

---
+++ WebScanner.py
@@ -0,0 +1,11 @@
+#reconnaissance scanner python > txtfile
+import os
+
+
+#store results is new directory
+def create_dir(directory):
+    if not os.path:exists(directory:)
+        os.makedirs(directory)
+
+
+#new file to store results
