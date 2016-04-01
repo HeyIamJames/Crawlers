809420dd2b3e6b915ed493578e74d6f04db13e3f
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-03-31 23:52:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import os
 host = IP
   if len(IP) > 16
   return None
+
+nma.scan(hosts='192.168.1.0/30', arguments='-sP', callback=callback_result)
+while nma.still_scanning():
+print("Waiting ...")
