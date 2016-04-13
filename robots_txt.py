1e6c0adb06e6daa9fc09ffd59d7d45f27a575bf4
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-04-12 19:39:38 -0700

    Update robots_txt.py

--- robots_txt.py
+++ robots_txt.py
@@ -1,12 +1,16 @@
 import urlib.request
 import io
 
-def ger_robots_txt(url):
+def get_robots_txt(url):
     if url.endswith('/')
         path = url
     else: 
         path - url + '/'
     req = urllib.request.urlopen(paht + "robots.txt", data=None)
+    data = io.TextIOWrapper(req, encoding = 'utf-8')
+    return data.read()
+
+print(get_robots_txt)
 
 # https://reddit.com/
 
